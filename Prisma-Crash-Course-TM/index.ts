5bf386bf60ec120a44ae101ea2247416d0a93831
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-22 22:47:28 +0600

    index.ts setup

--- Prisma-Crash-Course-TM/index.ts
+++ Prisma-Crash-Course-TM/index.ts
@@ -4,5 +4,25 @@ const prisma = new PrismaClient();
 
 
 async function main() {
+  // create a user
+  const user = await prisma.user.create({
+    data: {
+      name: 'Rashedin Islam',
+      email: 'rashedinislam.06@gmail',
+      
+    }
+  })
+
+  console.log(user)
   
 }
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e: Error) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
