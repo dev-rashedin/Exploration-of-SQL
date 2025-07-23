d0ca90eef561ad77d8d8457e0860d5f5f63dd576
Rashedin-063 <rashedinislam.06@gmail.com>
2025-07-23 08:43:51 +0600

    updated

--- Prisma-ORM-Full-Course-PedroTech/index.ts
+++ Prisma-ORM-Full-Course-PedroTech/index.ts
@@ -4,37 +4,9 @@ import { globalErrorHandler, notFoundHandler } from "express-error-toolkit";
 import { StatusCodes } from "http-status-toolkit";
 
 const app = express();
-const prisma = new PrismaClient();
 app.use(express.json());
 
-app.get("/users", async (_, res) => {
-  const users = await prisma.user.findMany({
-    where: {
-      nationality: {
-        in: ["Irish", "German", "Portuguese"],
-      },
-    },
-  });
-  res.status(StatusCodes.OK).json(users);
-});
 
-app.put("/users", async (_, res) => {
-  const updatedUser = await prisma.user.update({
-    where: { email: "pedro@example.com" },
-    data: {
-      age: 35,
-      isMarried: true,
-    },
-  });
-  res.status(StatusCodes.CREATED).json(updatedUser);
-});
-
-app.delete("/users", async (_, res) => {
-  const deletedUsers = await prisma.user.deleteMany({
-    where: { age: { gt: 30 } },
-  });
-  res.status(StatusCodes.OK).json(deletedUsers);
-});
 
 
 app.use(notFoundHandler);
